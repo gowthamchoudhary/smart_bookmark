274df6587c5ae4e80211ea8c9a454275c287e7ad
Gowtham <jayavarapugowtham@gmail.com>
2026-05-13 18:03:17 +0530

    edited the routes to adapt to new workspace system

--- backend/app/modules/bookmarks/routes.py
+++ backend/app/modules/bookmarks/routes.py
@@ -10,6 +10,8 @@ from app.modules.bookmarks.service import (
 )
 from app.modules.bookmarks.schema import BookmarkCreate, BookmarkResponse
 from app.modules.auth.service import get_current_user
+from app.models.workspace import Workspace
+from app.models.bookmark import Bookmark
 
 router = APIRouter(tags=["bookmarks"])
 
@@ -27,27 +29,28 @@ def create(
 
 
 @router.get("/", response_model=list[BookmarkResponse])
-def get_all(current_user=Depends(get_current_user), db: Session = Depends(get_db)):
+def get_all(workspace_id:int,current_user=Depends(get_current_user), db: Session = Depends(get_db)):
+    
     try:
-        return get_user_bookmarks(current_user, db)
+        return get_user_bookmarks(workspace_id,current_user, db)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.delete("/{bookmark_id}")
+@router.delete("/{workspace_id}/{bookmark_id}")
 def delete_route(
+    workspace_id:int,
     bookmark_id: int,
     current_user=Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    try:
-        delete_bookmark(bookmark_id, current_user, db)
+        delete_bookmark(workspace_id,bookmark_id, current_user, db)
         return {"message": "Deleted successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+   
 
-@router.get("/search")
+@router.get("/{workspace_id}/search")
 def search_bookmarks(
+    workspace_id:int,
     query: str,
     page: int = Query(1, ge=1),
     size: int = Query(10, ge=1, le=100),
@@ -58,6 +61,7 @@ def search_bookmarks(
     skip = (page - 1) * size
 
     bookmarks, total = search_bookmark(
+        workspace_id,
         current_user,
         size,
         skip,
@@ -73,15 +77,23 @@ def search_bookmarks(
         "pages": (total + size - 1) // size
     }
     
-@router.get("/paginated")
+@router.get("/{workspace_id}/paginated")
 def get_bookmarks(
+    workspace_id:int,
     page:int=Query(1,ge=1),
     size:int = Query(10,ge=1,le=100),
-    db:Session=Depends(get_db)
+    db:Session=Depends(get_db),
+    user = Depends(get_current_user)
 ):
+    db_workspace = db.query(Workspace).filter(Workspace.id==workspace_id).first()
+    if not db_workspace:
+        raise HTTPException(status_code=404,detail="not workspace exists")
+    if db_workspace.user_id!=user.id:
+        raise HTTPException(status_code=403,detail="user is not allowed")
+    
     offset = (page-1)*size
-    bookmarks = db.query(bookmarks).offset(offset).limit(size).all()
-    total = db.query(bookmarks).count()
+    bookmarks = db.query(Bookmark).filter(Bookmark.workspace_id==workspace_id).offset(offset).limit(size).all()
+    total = db.query(Bookmark).filter(Bookmark.workspace_id==workspace_id).count()
     return {
         "items":bookmarks,
         "page":page,

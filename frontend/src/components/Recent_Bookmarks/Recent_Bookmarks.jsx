1cb3c30e40e97cc1fa7cb5d66383060566727ea0
Gowtham <jayavarapugowtham@gmail.com>
2026-06-07 23:06:15 +0530

    top bar of the recent bookmarks section is done

--- frontend/src/components/Recent_Bookmarks/Recent_Bookmarks.jsx
+++ frontend/src/components/Recent_Bookmarks/Recent_Bookmarks.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import "./Recent_Bookmarks.css";
 import { FiChevronRight } from "react-icons/fi";
 import { FiMoreVertical } from "react-icons/fi";
-
+import { LuList } from "react-icons/lu";
 const Recent_Bookmarks = () => {
   return (
     <div className="recent_bookmarks">
       <div className="topbar_bookmarks">
         <p>Recent Bookmarks</p>
-        <div className="dropDown">
-          Recently Added{" "}
-          <span>
-            <FiChevronRight />
-          </span>
-        </div>
-        <div className="options">
-          <FiMoreVertical />
+        <div className="right_topbar">
+          <div className="dropDown">
+            Recently Added{" "}
+            <span>
+              <FiChevronRight />
+            </span>
+          </div>
+          <div className="options">
+            <div>
+              <LuList />
+            </div>
+          </div>
         </div>
       </div>
     </div>

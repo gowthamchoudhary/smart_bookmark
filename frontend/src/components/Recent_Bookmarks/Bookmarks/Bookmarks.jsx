ee3c1eef740d06bbc9098d159b244bdaadf2eb62
Gowtham <jayavarapugowtham@gmail.com>
2026-06-08 00:29:39 +0530

    added the recent bookmarks

--- frontend/src/components/Recent_Bookmarks/Bookmarks/Bookmarks.jsx
+++ frontend/src/components/Recent_Bookmarks/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
-const Bookmarks = () => {
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
+import "./Bookmarks.css";
+const Bookmarks = ({
+  icon,
+  title,
+  link,
+  workspace,
+  time,
+  color,
+  class_Name,
+}) => {
   return (
-    <div>
-      
+    <div className="bookmark">
+      <div className={`bookmark-icon ${class_Name}`}>{icon}</div>
+      <div className="bookmark-metadata">
+        <div className="title">{title}</div>
+        <a href={link} className="bookmark-url">
+          {link}
+        </a>
+      </div>
+      <div className={`workspace workspace-${color}`}>{workspace}</div>
+      <div className="time">{time}</div>
+      <div className="option">
+        <BsThreeDots />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bookmarks
+export default Bookmarks;
--- frontend/src/components/Recent_Bookmarks/Recent_Bookmarks.jsx
+++ frontend/src/components/Recent_Bookmarks/Recent_Bookmarks.jsx
@@ -3,6 +3,9 @@ import "./Recent_Bookmarks.css";
 import { FiChevronRight } from "react-icons/fi";
 import { FiMoreVertical } from "react-icons/fi";
 import { LuList } from "react-icons/lu";
+import Bookmarks from "./Bookmarks/Bookmarks";
+
+import { SiNotion, SiYoutube, SiGithub, SiYcombinator } from "react-icons/si";
 const Recent_Bookmarks = () => {
   return (
     <div className="recent_bookmarks">
@@ -22,6 +25,42 @@ const Recent_Bookmarks = () => {
           </div>
         </div>
       </div>
+      <Bookmarks
+        title="Building a second brain with Para method"
+        link="https://www.notion.com/"
+        icon={<SiNotion />}
+        workspace="Productivity"
+        time="2h ago"
+        class_Name="notion"
+        color="green"
+      />
+      <Bookmarks
+        title="Building a second brain with Para method"
+        link="https://www.notion.com/"
+        icon={<SiYoutube />}
+        workspace="Productivity"
+        class_Name="yt"
+        time="2h ago"
+        color="blue"
+      />
+      <Bookmarks
+        title="Building a second brain with Para method"
+        link="https://www.notion.com/"
+        icon={<SiGithub />}
+        workspace="Productivity"
+        class_Name="git"
+        time="2h ago"
+        color="pink"
+      />
+      <Bookmarks
+        title="Building a second brain with Para method"
+        link="https://www.notion.com/"
+        class_Name="yc"
+        icon={<SiYcombinator />}
+        workspace="Productivity"
+        time="2h ago"
+        color="orange"
+      />
     </div>
   );
 };
